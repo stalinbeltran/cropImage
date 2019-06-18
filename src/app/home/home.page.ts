a68593c0079c44f0beb2a810396bdbb3208af5db
Stalin Beltran <stalin.beltran2006@gmail.com>
2019-06-18 18:27:16 -0500

    funciona carga de imagen via input

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { ViewChild } from '@angular/core';
 import { AngularCropperjsComponent } from 'angular-cropperjs';
 
@@ -13,8 +13,13 @@ import { NgxCropperjsComponent } from 'ngx-cropperjs';
 })
 export class HomePage {
   @ViewChild('angularCropper') public angularCropper: NgxCropperjsComponent;
-  imageUrl = "./assets/paisaje.jpg"
+  // @ViewChild('imageFile') public angularCropper: NgxCropperjsComponent;
+  @ViewChild('imageFile') imageFilePath: ElementRef;
+  @ViewChild('fileLoader') fileLoader: ElementRef;
 
+  
+  myImage = "./assets/paisaje.jpg"
+  uploadImage = undefined
   config = {
     dragMode: 'crop',
     aspectRatio: 1,
@@ -26,12 +31,144 @@ export class HomePage {
   };
   cropperOptions: any;
   croppedImage = null; 
-  myImage = null;
+  // myImage = null;
   scaleValX = 1;
   scaleValY = 1;
+  processing = false
+
 
   constructor(
   ) {}
+
+  imageFilePath_change($event){
+    let filel:any = this.fileLoader.nativeElement
+    console.log("filel")
+    console.log(filel)
+    // var file = filel.files[0];
+    /*
+    console.log($event)
+    let file = $event.target.files[0];
+    // let file = this.imageFilePath.nativeElement.files[0]
+    console.log(file)*/
+  }
+  // imageFilePath = ""
+
+  presentActionSheet(fileLoader) {
+    fileLoader.click();
+    var that = this;
+    fileLoader.onchange = function () {
+      var file = fileLoader.files[0];
+      var reader = new FileReader();
+
+      reader.addEventListener("load", function () {
+        that.processing = true;
+        that.getOrientation(fileLoader.files[0], function (orientation) {
+          if (orientation > 1) {
+            that.resetOrientation(reader.result, orientation, function (resetBase64Image) {
+              that.uploadImage = resetBase64Image;
+            });
+          } else {
+            that.uploadImage = reader.result;
+          }
+        });
+      }, false);
+
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+    }
+  }
+
+
+
+
+  imageLoaded(){
+    this.processing = false;
+  }
+  getOrientation(file, callback) {
+    var reader = new FileReader();
+    reader.onload = function (e:any) {
+
+      var view = new DataView(e.target.result);
+      if (view.getUint16(0, false) != 0xFFD8) return callback(-2);
+      var length = view.byteLength, offset = 2;
+      while (offset < length) {
+        var marker = view.getUint16(offset, false);
+        offset += 2;
+        if (marker == 0xFFE1) {
+          if (view.getUint32(offset += 2, false) != 0x45786966) return callback(-1);
+          var little = view.getUint16(offset += 6, false) == 0x4949;
+          offset += view.getUint32(offset + 4, little);
+          var tags = view.getUint16(offset, little);
+          offset += 2;
+          for (var i = 0; i < tags; i++)
+            if (view.getUint16(offset + (i * 12), little) == 0x0112)
+              return callback(view.getUint16(offset + (i * 12) + 8, little));
+        }
+        else if ((marker & 0xFF00) != 0xFF00) break;
+        else offset += view.getUint16(offset, false);
+      }
+      return callback(-1);
+    };
+    reader.readAsArrayBuffer(file);
+  }
+  resetOrientation(srcBase64, srcOrientation, callback) {
+    var img = new Image();
+
+    img.onload = function () {
+      var width = img.width,
+        height = img.height,
+        canvas = document.createElement('canvas'),
+        ctx = canvas.getContext("2d");
+
+      // set proper canvas dimensions before transform & export
+      if (4 < srcOrientation && srcOrientation < 9) {
+        canvas.width = height;
+        canvas.height = width;
+      } else {
+        canvas.width = width;
+        canvas.height = height;
+      }
+
+      // transform context before drawing image
+      switch (srcOrientation) {
+        case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+        case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
+        case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
+        case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+        case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
+        case 7: ctx.transform(0, -1, -1, 0, height, width); break;
+        case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+        default: break;
+      }
+
+      // draw image
+      ctx.drawImage(img, 0, 0);
+
+      // export base64
+      callback(canvas.toDataURL());
+    };
+
+    img.src = srcBase64;
+  }
+  removePic() {
+    this.uploadImage = null;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -53,24 +190,24 @@ export class HomePage {
     this.angularCropper.cropper.zoom(factor);
   }
  
-  // scaleX() {
-  //   this.scaleValX = this.scaleValX * -1;
-  //   this.angularCropper.cropper.scaleX(this.scaleValX);
-  // }
+  scaleX() {
+    this.scaleValX = this.scaleValX * -1;
+    this.angularCropper.cropper.scaleX(this.scaleValX);
+  }
  
-  // scaleY() {
-  //   this.scaleValY = this.scaleValY * -1;
-  //   this.angularCropper.cropper.scaleY(this.scaleValY);
-  // }
+  scaleY() {
+    this.scaleValY = this.scaleValY * -1;
+    this.angularCropper.cropper.scaleY(this.scaleValY);
+  }
  
-  // move(x, y) {
-  //   this.angularCropper.cropper.move(x, y);
-  // }
+  move(x, y) {
+    this.angularCropper.cropper.move(x, y);
+  }
  
-  // save() {
-  //   let croppedImgB64String: string = this.angularCropper.cropper.getCroppedCanvas().toDataURL('image/jpeg', (100 / 100));
-  //   this.croppedImage = croppedImgB64String;
-  // }
+  save() {
+    let croppedImgB64String: string = this.angularCropper.cropper.getCroppedCanvas().toDataURL('image/jpeg', (100 / 100));
+    this.croppedImage = croppedImgB64String;
+  }
 
 
 
